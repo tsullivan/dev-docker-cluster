b945e83b91ea233d79e6a82673e83056708b741e
Timothy Sullivan <tsullivan@elastic.co>
2022-08-02 09:44:10 -0700

    discover_read_only_user

--- create-users.sh
+++ create-users.sh
@@ -26,7 +26,15 @@ send_request /_security/role/dev_reporting_user '
 { "applications": [ { "application": "kibana-.kibana", "privileges": [ "feature_discover.minimal_read", "feature_discover.generate_report", "feature_dashboard.minimal_read", "feature_dashboard.generate_report", "feature_dashboard.download_csv_report", "feature_canvas.read", "feature_canvas.generate_report", "feature_visualize.minimal_read", "feature_visualize.generate_report" ], "resources": [ "*" ] } ], "cluster": [], "indices": [], "metadata": {}, "run_as": [], "transient_metadata": { "enabled": true } }
 '
 
+send_request /_security/role/discover_read_only '
+{"metadata":{},"elasticsearch":{"cluster":[],"indices":[{"names":["kibana_sample_data_ecommerce"],"privileges":["read","view_index_metadata"],"allow_restricted_indices":false}],"run_as":[]},"kibana":[{"spaces":["*"],"base":["read"],"feature":{}}]}
+'
+
 # useres
 send_request /_security/user/test_user '
 {"email":"test@example.com","enabled":true,"full_name":"Test T. User","metadata":{},"password":"changeme","roles":["data_user","dev_reporting_user"]}
+'
+
+send_request /_security/user/discover_read_only_user '
+{"password":"changeme","username":"discover_read_only_user","full_name":"D","email":"asdoifj@gsajdhfa.did","roles":["discover_read_only"]}
 '
